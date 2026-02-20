ce9a7be75b9ef101cbabac2edbc4ae89e43dee04
LucasGb <lucas.macielbarros0@gmail.com>
2026-02-19 23:56:03 -0300

    feat: Criação da classe Pagamentos

---
+++ model/Pagamentos.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Pagamentos {
+    private Aluno aluno;
+    private Plano valor;
+    private int dataDia;
+    private int dataMes;
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+    public Plano getValor() {
+        return valor;
+    }
+    public void setValor(Plano valor) {
+        this.valor = valor;
+    }
+    public int getDataDia() {
+        return dataDia;
+    }
+    public void setDataDia(int dataDia) {
+        this.dataDia = dataDia;
+    }
+    public int getDataMes() {
+        return dataMes;
+    }
+    public void setDataMes(int dataMes) {
+        this.dataMes = dataMes;
+    }
+
+    
+
+}
